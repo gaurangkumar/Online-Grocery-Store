44279171c7b5ebed2dc6f2b9493a2d047bff1a31
Himani-14p <himaniasoda1999@gmail.com>
2020-10-09 23:17:05 +0530

    update

---
+++ admin/orders.php
@@ -0,0 +1,162 @@
+<?php
+session_start();
+if (!isset($_SESSION['ADMIN_ID']) || empty($_SESSION['ADMIN_ID'])) {
+    header('Location: login.php');
+    exit;
+}
+
+require '../dbcon.php';
+
+$result = $conn->query('SELECT * FROM `ord` ORDER BY `oid` DESC');
+$total_ord = $result->num_rows;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php
+    $title = 'All Order | Admin';
+    require 'include/head.php';
+?>
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+<?php
+    require 'include/sidebar.php';
+?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+<?php
+    require 'include/topbar.php';
+?>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">All Order (<?=$total_ord?>)</h1>
+            <a href="category-add.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+            	<i class="fa fa-plus-circle"></i> Create New
+            </a>
+          </div>
+
+          <!-- Content Row -->
+            <div class="row">
+                <!-- column -->
+                <div class="col-12">
+                    <div class="form-group m-t-40">
+                        <?php
+                        if (!isset($_SESSION['msg']) || $_SESSION['msg'] == '') {
+                        } else {
+                            ?>
+				        <div class="alert alert-<?=$_SESSION['msg']['type']?> alert-dismissable">
+					        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+					        <?=$_SESSION['msg']['msg']?>
+				        </div>
+                        <?php
+                            $_SESSION['msg'] = '';
+                            unset($_SESSION['msg']);
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title"> </h4>
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Oid</th>
+                                            <th>Date</th>
+                                            <th>Qunatity</th>
+                                            <th>Pid</th>
+                                            <th>Uid</th>
+                                            <th class="text-nowrap">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        if ($total_ord) {
+                                            while ($row = $result->fetch_assoc()) {
+                                                $result1 = $conn->query("SELECT `name` FROM `user` WHERE `uid` = $row[uid]");
+                                                $ord = $result1->fetch_assoc();
+												 $result1 = $conn->query("SELECT `name` FROM `product` WHERE `pid` = $row[pid]");
+                                                $ord1 = $result1->fetch_assoc(); ?>
+                                        <tr>
+                                            <td><?=$row['oid']?></td>
+                                            <td><?=$row['date']?></td>
+                                            <td><?=$row['quntity']?></td>
+                                            <td><?=$ord['name']?></td>
+                                            <td><?=$ord1['name']?></td>
+                                            <td class="text-nowrap">
+                                                
+                                                
+                                            </td>
+                                        </tr>
+										<?php
+                                            }
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- column -->
+            </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+         </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2020</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+<?php
+    require 'include/javascript.php';
+?>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
