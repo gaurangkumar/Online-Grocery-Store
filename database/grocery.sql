e79d7d7c9c5f49af38a74aed483ac608c7c4bf27
gaurangkumar <gaurangkumarp@gmail.com>
2020-10-12 11:24:41 +0530

    db changed

--- database/grocery.sql
+++ database/grocery.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2020 at 11:18 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 5.6.31
+-- Generation Time: Oct 12, 2020 at 07:52 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `grocery`
 --
+CREATE DATABASE IF NOT EXISTS `grocery` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `grocery`;
 
 -- --------------------------------------------------------
 
@@ -28,11 +29,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
+DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
   `aid` int(10) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL
+  `password` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -40,7 +42,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`aid`, `name`, `email`, `password`) VALUES
-(1, 'himani', 'himaniaasoda1999@gmail.com', '8814');
+(1, 'himani', 'himaniaasoda1999@gmail.com', 8814);
 
 -- --------------------------------------------------------
 
@@ -48,6 +50,7 @@ INSERT INTO `admin` (`aid`, `name`, `email`, `password`) VALUES
 -- Table structure for table `category`
 --
 
+DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `cid` int(10) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
@@ -74,23 +77,14 @@ INSERT INTO `category` (`cid`, `name`, `parent_id`) VALUES
 -- Table structure for table `feedback`
 --
 
+DROP TABLE IF EXISTS `feedback`;
 CREATE TABLE `feedback` (
   `fid` int(10) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `mobile` varchar(12) DEFAULT NULL,
-  `msg` varchar(100) DEFAULT NULL,
+  `msg` text DEFAULT NULL,
   `uid` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `feedback`
---
-
-INSERT INTO `feedback` (`fid`, `name`, `mobile`, `msg`, `uid`) VALUES
-(1, 'dax', '9106682997', 'good', NULL),
-(2, 'dax', '9106682997', 'good', NULL),
-(3, 'dax', '9106682997', 'good', NULL),
-(4, 'dax', '9106682997', 'good', NULL);
 
 -- --------------------------------------------------------
 
@@ -98,12 +92,28 @@ INSERT INTO `feedback` (`fid`, `name`, `mobile`, `msg`, `uid`) VALUES
 -- Table structure for table `ord`
 --
 
+DROP TABLE IF EXISTS `ord`;
 CREATE TABLE `ord` (
   `oid` int(10) NOT NULL,
-  `pid` int(10) DEFAULT NULL,
-  `uid` int(10) DEFAULT NULL,
-  `date` datetime(6) DEFAULT NULL,
-  `quntity` int(10) DEFAULT NULL
+  `uid` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+DROP TABLE IF EXISTS `order_items`;
+CREATE TABLE `order_items` (
+  `item_id` int(11) NOT NULL,
+  `oid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `subtotal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -112,12 +122,13 @@ CREATE TABLE `ord` (
 -- Table structure for table `payment`
 --
 
+DROP TABLE IF EXISTS `payment`;
 CREATE TABLE `payment` (
   `payid` int(10) NOT NULL,
-  `total amount` int(20) DEFAULT NULL,
-  `paymenttype` varchar(50) DEFAULT NULL,
   `oid` int(10) DEFAULT NULL,
-  `uid` int(10) DEFAULT NULL
+  `uid` int(10) DEFAULT NULL,
+  `total_amount` int(11) DEFAULT NULL,
+  `payment_type` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -126,6 +137,7 @@ CREATE TABLE `payment` (
 -- Table structure for table `product`
 --
 
+DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
   `pid` int(10) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
@@ -159,15 +171,16 @@ INSERT INTO `product` (`pid`, `name`, `price`, `discount`, `weight`, `pic`, `cid
 (16, 'Masur dal', 60, 65, '(1 kg)', 'images/masur.jpg', 7),
 (17, 'Chana dal', 40, 50, '(500 gm)', 'images/chanadal.jpg', 7),
 (18, 'Chana', 50, 55, '(1 kg)', 'images/chana.jpg', 8),
-(19, 'Mug', 40, 55, '1 kg', 'images/mung.jpg', 8),
-(25, 'clinic plush', 50, 54, '700 ml', 'images/1602260557-clinic.jpg', 2),
-(26, 'chik', 50, 54, '700 ml', 'images/1602266961-chick.jpg', 2),
-(27, 'Dettol', 40, 0, '4 pc', 'images/1602267040-d3.jpg', 2),
-(28, 'Dettol Handwash', 30, 40, '250ml', 'images/1602267137-dh.jpg', 2),
-(29, 'lifebuoy', 50, 50, '4 pc', 'images/1602267355-life.jpg', 2),
-(31, 'surf excel', 150, 160, '1kg', 'images/1602267705-surf2.jpg', 3),
-(33, 'Nirma', 40, 50, '1 kg', 'images/1602267875-nirma.jpg', 3),
-(38, 'brinjal', 40, 50, '1kg', 'images/1602268367-31.png', 4);
+(19, 'Mug', 40, 50, '(1 kg)', 'images/mung.jpg', 8),
+(20, 'Urad', 100, 10, '(1 kg)', 'images/ur.jpg', 8),
+(21, 'clinic plush', 50, 54, '700 ml', 'images/1602260557-clinic.jpg', 2),
+(22, 'chik', 50, 54, '700 ml', 'images/1602266961-chick.jpg', 2),
+(23, 'Dettol', 40, 0, '4 pc', 'images/1602267040-d3.jpg', 2),
+(24, 'Dettol Handwash', 30, 40, '250ml', 'images/1602267137-dh.jpg', 2),
+(25, 'lifebuoy', 50, 50, '4 pc', 'images/1602267355-life.jpg', 2),
+(26, 'surf excel', 150, 160, '1kg', 'images/1602267705-surf2.jpg', 3),
+(27, 'Nirma', 40, 50, '1 kg', 'images/1602267875-nirma.jpg', 3),
+(28, 'brinjal', 40, 50, '1kg', 'images/1602268367-31.png', 4);
 
 -- --------------------------------------------------------
 
@@ -175,13 +188,14 @@ INSERT INTO `product` (`pid`, `name`, `price`, `discount`, `weight`, `pic`, `cid
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `uid` int(10) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `mobile` varchar(10) NOT NULL,
+  `mobile` bigint(10) NOT NULL,
   `address1` varchar(50) NOT NULL,
-  `gender` enum('male','female') DEFAULT NULL,
-  `username` varchar(50) NOT NULL,
+  `gender` enum('male','female') NOT NULL,
+  `username` varchar(20) NOT NULL,
   `password` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -190,11 +204,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`uid`, `name`, `mobile`, `address1`, `gender`, `username`, `password`) VALUES
-(32, 'himani', '9106682997', 'visnager, VILLAGE=ASODA', '', 'himani', 'hd6034'),
-(33, 'divya', '9979331605', 'visnager, VILLAGE=ASODA', '', 'daxrp', 'dk7845'),
-(34, 'kishan', '9714304713', 'visnager', '', 'kishan rp', 'kr9856'),
-(35, 'rajubhai', '9979331605', 'asoda', '', 'daxrp', 'rp1236'),
-(36, 'sonali', '1236547898', 'visnager,', '', 'sonali', 'sp1236');
+(1, 'himani', 9106682997, 'visnager, VILLAGE=ASODA', 'female', 'himani', 'hd6034'),
+(2, 'divya', 9979331605, 'visnager, VILLAGE=ASODA', 'female', 'daxrp', 'dk7845'),
+(3, 'kishan', 9714304713, 'visnager', 'male', 'kishan rp', 'kr9856'),
+(4, 'rajubhai', 9979331605, 'asoda', 'male', 'daxrp', 'rp1236'),
+(5, 'sonali', 1236547898, 'visnager,', 'female', 'sonali', 'sp1236');
 
 --
 -- Indexes for dumped tables
@@ -224,8 +238,15 @@ ALTER TABLE `feedback`
 --
 ALTER TABLE `ord`
   ADD PRIMARY KEY (`oid`),
-  ADD KEY `uid` (`uid`),
-  ADD KEY `pid` (`pid`);
+  ADD KEY `user_id` (`uid`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `order_id` (`oid`),
+  ADD KEY `product_id` (`pid`);
 
 --
 -- Indexes for table `payment`
@@ -257,52 +278,59 @@ ALTER TABLE `user`
 --
 ALTER TABLE `admin`
   MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `cid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT for table `feedback`
---
-ALTER TABLE `feedback`
-  MODIFY `fid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `cid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `ord`
 --
 ALTER TABLE `ord`
   MODIFY `oid` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
   MODIFY `payid` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `pid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `pid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `uid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `feedback`
---
-ALTER TABLE `feedback`
-  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `ord`
 --
 ALTER TABLE `ord`
-  ADD CONSTRAINT `ord_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `ord_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `product` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`);
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_id` FOREIGN KEY (`oid`) REFERENCES `ord` (`oid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_id` FOREIGN KEY (`pid`) REFERENCES `product` (`pid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `payment`
